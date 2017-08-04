e14bb341a884d04831d9640a8395f7518bb0c2bf
Kamil Ciemniewski <kamil@ciemniew.ski>
2017-08-04 14:03:42 +0200

    Matrix initialization

--- src/matrix_helpers.cpp
+++ src/matrix_helpers.cpp
@@ -4,8 +4,29 @@ namespace mozart {
     template<typename T>
     matrix<T> make_matrix(initializer_list<initializer_list<T>> values)
     {
-        // todo: implement me
-        return matrix<T>(0, 0);
+        auto rows = 0;
+        auto cols = 0;
+
+        for(auto r : values) {
+            ++rows;
+            for(auto c: r) {
+                ++cols;
+            }
+        }
+
+        auto mat = matrix<T>(rows, cols);
+
+        size_t rindex = 0;
+        size_t cindex = 0;
+        for(auto r  = values.begin(); r < values.end(); r++) {
+            for(auto c = (*r).begin(); c < (*r).end(); c++) {
+                mat(rindex, cindex) = *c;
+            }
+            ++rindex;
+            ++cindex;
+        }
+        
+        return mat;
     }
 
     template matrix<float> make_matrix(initializer_list<initializer_list<float>> values);
