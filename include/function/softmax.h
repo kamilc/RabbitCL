29647914fcdd9d7c3f8150c3a6fb1031d3a58db5
Kamil Ciemniewski <kamil@ciemniew.ski>
2017-08-17 10:48:01 +0200

    Refactored Softmax

--- include/function/softmax.h
+++ include/function/softmax.h
@@ -1,16 +1,13 @@
 #ifndef Softmax_h
 #define Softmax_h
 
-#include <tuple>
-#include "boost/optional.hpp"
 #include "viennacl/matrix.hpp"
 #include "viennacl/ocl/local_mem.hpp"
-#include "viennacl/backend/memory.hpp"
 #include "utilities.h"
 #include "activation.h"
+#include "kernel_class.h"
 
 using namespace std;
-using namespace boost;
 using namespace viennacl;
 using namespace viennacl::ocl;
 using namespace viennacl::backend;
@@ -21,6 +18,9 @@ namespace mozart
     {
         template<typename T>
         activation<T> softmax(matrix<T>& in, bool derive);
+
+        KERNEL_CLASS(softmax_kernel);
+        KERNEL_CLASS(softmax_deriv_kernel);
     }
 }
 
--- src/function/softmax.cpp
+++ src/function/softmax.cpp
@@ -4,57 +4,85 @@ namespace mozart
 {
     namespace function
     {
-        inline ocl::kernel& get_softmax_kernel()
+        
+        template<typename T>
+        inline const char * softmax_kernel<T>::name()
         {
-            static const char * softmax_ocl_program =
-            "__kernel void softmax(\n"
-            "          __global float * in,\n"
-            "          __global float * out,\n"
-            "          __local  float * buff,\n"
-            "           unsigned int isize1,\n"
-            "           unsigned int isize2)\n"
-            "{ \n"
-            "  unsigned int gid = get_global_id(0);\n"
-            "  unsigned int lid = get_local_id(0);\n"
-            "  unsigned int group_size = get_local_size(0);\n"
-            "  unsigned int padded = isize1 - group_size;\n"
-            "  unsigned int row = gid / group_size;\n"
-            "  unsigned int idx = gid + row*padded;\n"
-            "  buff[lid] = exp(in[idx]);\n"
-            "  barrier(CLK_LOCAL_MEM_FENCE);\n"
-            "  for(int i = (group_size+1)/2; i>0; i >>= 1) {\n"
-            "    if(lid < i) {\n"
-            "      buff[lid] += buff[lid + i];\n"
-            "    }\n"
-            "    barrier(CLK_LOCAL_MEM_FENCE);\n"
-            "  }\n"
-            "  out[idx] = exp(in[idx]) / buff[0];\n"
-            "};\n";
-
-            auto &softmax_ocl =
-                ocl::current_context().add_program(softmax_ocl_program, "softmax");
-            return softmax_ocl.get_kernel("softmax");
+            return "softmax_kernel";
         }
 
-        inline ocl::kernel& get_softmax_deriv_kernel()
+        template<typename T>
+        inline const char * softmax_kernel<T>::code()
         {
-            static const char * softmax_deriv_ocl_program =
-                "__kernel void softmax_deriv(\n"
-                "          __global float * in,\n"
-                "          __global float * out,\n"
-                "              unsigned int size1,\n"
-                "              unsigned int size2,\n"
-                "              unsigned int isize1)\n"
-                "{ \n"
-                "  unsigned int gid = get_global_id(0);\n"
-                "  unsigned int padded = isize1 - size1;\n"
-                "  unsigned int row = gid / size2;\n"
-                "  unsigned int idx = gid + row*padded;\n"
-                "  out[idx] = in[idx] * (1.0f - in[idx]);\n"
-                "};\n";
-            auto &softmax_deriv_ocl =
-            ocl::current_context().add_program(softmax_deriv_ocl_program, "softmax_deriv");
-            return softmax_deriv_ocl.get_kernel("softmax_deriv");
+            return  "__kernel void softmax_kernel(\n"
+                    "          __global float * in,\n"
+                    "          __global float * out,\n"
+                    "          __local  float * buff,\n"
+                    "           unsigned int isize1,\n"
+                    "           unsigned int isize2)\n"
+                    "{ \n"
+                    "  unsigned int gid = get_global_id(0);\n"
+                    "  unsigned int lid = get_local_id(0);\n"
+                    "  unsigned int group_size = get_local_size(0);\n"
+                    "  unsigned int padded = isize1 - group_size;\n"
+                    "  unsigned int row = gid / group_size;\n"
+                    "  unsigned int idx = gid + row*padded;\n"
+                    "  buff[lid] = exp(in[idx]);\n"
+                    "  barrier(CLK_LOCAL_MEM_FENCE);\n"
+                    "  for(int i = (group_size+1)/2; i>0; i >>= 1) {\n"
+                    "    if(lid < i) {\n"
+                    "      buff[lid] += buff[lid + i];\n"
+                    "    }\n"
+                    "    barrier(CLK_LOCAL_MEM_FENCE);\n"
+                    "  }\n"
+                    "  out[idx] = exp(in[idx]) / buff[0];\n"
+                    "};\n";
+        }
+
+        template<typename T>
+        void softmax_kernel<T>::compute_matrix(matrix<T>& in, matrix<T>& out)
+        {
+            ocl::enqueue(this->_kernel( in,
+                                        out,
+                                        local_mem(in.size2()),
+                                        cl_uint(out.internal_size1()),
+                                        cl_uint(out.internal_size2())));
+            finish();
+        }
+
+        template<typename T>
+        inline const char * softmax_deriv_kernel<T>::name()
+        {
+            return "softmax_deriv_kernel";
+        }
+
+        template<typename T>
+        inline const char * softmax_deriv_kernel<T>::code()
+        {
+            return  "__kernel void softmax_deriv_kernel(\n"
+                    "          __global float * in,\n"
+                    "          __global float * out,\n"
+                    "              unsigned int size1,\n"
+                    "              unsigned int size2,\n"
+                    "              unsigned int isize1)\n"
+                    "{ \n"
+                    "  unsigned int gid = get_global_id(0);\n"
+                    "  unsigned int padded = isize1 - size1;\n"
+                    "  unsigned int row = gid / size2;\n"
+                    "  unsigned int idx = gid + row*padded;\n"
+                    "  out[idx] = in[idx] * (1.0f - in[idx]);\n"
+                    "};\n";
+        }
+
+        template<typename T>
+        void softmax_deriv_kernel<T>::compute_matrix(matrix<T>& in, matrix<T>& out)
+        {
+            ocl::enqueue(this->_kernel( in,
+                                        out,
+                                        cl_uint(out.size1()),
+                                        cl_uint(out.size2()),
+                                        cl_uint(out.internal_size1())));
+            finish();
         }
 
         template<typename T>
@@ -62,30 +90,17 @@ namespace mozart
         {
             activation<T> result(in, derive);
 
-            ocl::kernel &softmax_kernel = get_softmax_kernel();
-            softmax_kernel.local_work_size(0, in.size2());
-            softmax_kernel.global_work_size(0, in.size2() * in.size1());
-            ocl::enqueue(softmax_kernel(in,
-                                        result.out,
-                                        local_mem(in.size2()),
-                                        cl_uint(result.out.internal_size1()),
-                                        cl_uint(result.out.internal_size2())));
-            finish();
+            softmax_kernel<T>::run_matrix(in, result.out, in.size1(), in.size2() * in.size1());
 
             if(derive) {
-                ocl::kernel &softmax_deriv_kernel = get_softmax_deriv_kernel();
-                softmax_deriv_kernel.local_work_size(0, in.size1());
-                softmax_deriv_kernel.global_work_size(0, in.size2() * in.size1());
-                ocl::enqueue(softmax_deriv_kernel(result.out,
-                                                  result.deriv,
-                                                  cl_uint(result.deriv.size1()),
-                                                  cl_uint(result.deriv.size2()),
-                                                  cl_uint(result.deriv.internal_size1())));
-                finish();
+                softmax_deriv_kernel<T>::run_matrix(result.out, result.deriv, in.size1(), in.size2() * in.size1());
             }
 
             return result;
         }
+
+        INSTANTIATE(softmax_kernel);
+        INSTANTIATE(softmax_deriv_kernel);
 
         template activation<float> softmax(matrix<float>& in, bool derive);
         template activation<double> softmax(matrix<double>& in, bool derive);
